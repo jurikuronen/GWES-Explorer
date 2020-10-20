c5ca00d4b50d08adb7cdac0a39040e154eb4dcfd
Juri Kuronen <juri.kuronen@gmail.com>
2020-10-20 12:15:06 +0200

    Fix strange R BUILD problem.

--- R/ui.R
+++ R/ui.R
@@ -1,3 +1,13 @@
+# Build fails if these are not called here. I have no idea why.
+source("R/ui_about_panel.R")
+source("R/ui_analyse_panel.R")
+source("R/ui_circular_plot_panel.R")
+source("R/ui_data_panel.R")
+source("R/ui_data_sidebar.R")
+source("R/ui_gwes_manhattan_plot_panel.R")
+source("R/ui_phylogenetic_tree_plot_panel.R")
+source("R/ui_utils.R")
+
 # Define UI for application
 .ui <- shiny::fluidPage(
     shinyjs::useShinyjs(),
