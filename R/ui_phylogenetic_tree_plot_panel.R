d9d553583112d946e29313b02ef36c742756fd60
Juri Kuronen <juri.kuronen@gmail.com>
2020-10-26 14:33:45 +0100

    Fix tree settings not showing.

--- R/ui_phylogenetic_tree_plot_panel.R
+++ R/ui_phylogenetic_tree_plot_panel.R
@@ -6,13 +6,13 @@
         .div_inline_block(4, .prettySwitch("show_tree_plot_label_prop", "Column labels")),
         .div_inline_block(4, .prettySwitch("show_tree_plot_legend_prop", "Legend")),
         shiny::conditionalPanel(
-            condition = "input.tree_plot_show_fig_size",
+            condition = "input.show_tree_plot_fig_size",
             .div_inline_block(6, shiny::sliderInput("tree_plot_width", "Figure width (cm):", min = 10, max = 30, value = 20)),
             .div_inline_br_block(0.5),
             .div_inline_block(6, shiny::sliderInput("tree_plot_height", "Figure height (cm):", min = 10, max = 30, value = 20))
         ),
         shiny::conditionalPanel(
-            condition = "input.tree_plot_show_label_prop",
+            condition = "input.show_tree_plot_label_prop",
             .div_inline_block(6, shiny::sliderInput("tree_label_angle", "Column label angle:", min = 0, max = 90, value = 30)),
             .div_inline_br_block(0.5),
             .div_inline_block(6, shiny::sliderInput("tree_label_fs", "Column label font size:", min = 2, max = 10, value = 5)),
@@ -22,7 +22,7 @@
             .div_inline_block(6, shiny::sliderInput("tree_label_offset_y", "Column label offset Y:", min = 0, max = 200, value = 100))
         ),
         shiny::conditionalPanel(
-            condition = "input.tree_plot_show_legend_prop",
+            condition = "input.show_tree_plot_legend_prop",
             .div_inline_block(6, shiny::sliderInput("tree_legend_fs", "Legend font size:", min = 8, max = 20, value = 14)),
             .div_inline_br_block(0.5),
             .div_inline_block(6, shiny::sliderInput("tree_legend_size", "Legend size:", min = 0.5, max = 2, value = 1.4))
