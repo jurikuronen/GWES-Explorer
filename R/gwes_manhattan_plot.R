e53c7f91280bd130ff5bd8767dcb38d79162bbd9
Juri Kuronen <juri.kuronen@gmail.com>
2020-10-27 14:10:25 +0100

    Add settings for GWES Manhattan plot.

--- R/gwes_manhattan_plot.R
+++ R/gwes_manhattan_plot.R
@@ -5,19 +5,40 @@
         max_mi <- max(.data$outliers$MI)
         max_distance <- max(.data$outliers$Distance)
         return(shiny::renderPlot({
-            ggplot(data = .data$outliers, mapping = aes(x = Distance, y = MI, group = Direct, fontsize(12))) +
-                geom_point(aes(color = Direct), size = 0.5) +
-                geom_point(data = .data$outliers_direct[input$outliers_table_rows_selected, ], size = 5, color = "red", shape = 1) +
-                scale_colour_manual(values=c("grey", "black")) +
+            ggplot(
+                data = dplyr::arrange(.data$outliers, Direct), # Sort data such that direct outliers are plotted in the last layer.
+                mapping = aes(x = Distance, y = MI, group = Direct)
+            ) + geom_point(aes(color = Direct, size = Direct)) +
+                geom_point(
+                    data = .data$outliers_direct[input$outliers_table_rows_selected, ],
+                    size = input$gwes_selection_size,
+                    color = input$gwes_selection_color,
+                    shape = 1
+                ) +
+                scale_size_manual(
+                    values = c(size = input$gwes_size_indirect, input$gwes_size_direct)
+                ) +
+                scale_colour_manual(
+                    values = c(input$gwes_color_indirect, input$gwes_color_direct)
+                ) +
                 geom_line(aes(y = min_mi), color = "black") +
                 scale_x_continuous(limits = c(0, max_distance), expand = c(0, 0)) +
                 scale_y_continuous(limits = c(min_mi, max_mi + 0.05), expand = c(0, 0)) +
-                theme(text = element_text(size = 14)) +
-                coord_cartesian(xlim = mh_gwes_ranges$x, ylim = mh_gwes_ranges$y, expand = FALSE)
+                theme(
+                    axis.text = element_text(size = input$gwes_axis_size),
+                    axis.title = element_text(size = input$gwes_title_size),
+                    legend.text = element_text(size = input$gwes_legend_size),
+                    legend.title = element_text(size = input$gwes_legend_size)
+                ) +
+                coord_cartesian(
+                    xlim = mh_gwes_ranges$x,
+                    ylim = mh_gwes_ranges$y,
+                    expand = FALSE
+                )
         }))
     } else return(NULL)
 }
 
-.render_gwes_manhattan_plot_table <- function(input, outlier_columns) { 
+.render_gwes_manhattan_plot_table <- function(input, outlier_columns) {
     return(shiny::renderTable({shiny::nearPoints(.data$outliers_direct, input$manhattan_plot_click, addDist = TRUE)[, outlier_columns]}))
 }
--- R/ui_gwes_manhattan_plot_panel.R
+++ R/ui_gwes_manhattan_plot_panel.R
@@ -1,6 +1,36 @@
 .gwes_plot_panel <- function() {
     shiny::tabPanel("GWES Manhattan",
-                    shiny::br(), shiny::br(), shiny::br(), shiny::br(), shiny::br(),
+                    shiny::br(),
+                    .div_inline_block(3, "Modify figure:"),
+                    .div_inline_block(4, .prettySwitch("show_gwes_plot_marker_size", "Marker size")),
+                    .div_inline_block(4, .prettySwitch("show_gwes_plot_text_size", "Text size")),
+                    .div_inline_block(4, .prettySwitch("show_gwes_plot_colors", "Color")),
+                    shiny::conditionalPanel(
+                        condition = "input.show_gwes_plot_marker_size",
+                        .div_inline_block(6, shiny::sliderInput("gwes_size_direct", "Direct point size:", min = 0.05, max = 5, value = 1, step = 0.05)),
+                        .div_inline_br_block(0.5),
+                        .div_inline_block(6, shiny::sliderInput("gwes_size_indirect", "Indirect point size:", min = 0.05, max = 5, value = 0.5, step = 0.05)),
+                        .div_inline_br_block(0.5),
+                        .div_inline_block(6, shiny::sliderInput("gwes_selection_size", "Selection size:", min = 0.05, max = 20, value = 5, step = 0.1)),
+                    ),
+                    shiny::conditionalPanel(
+                        condition = "input.show_gwes_plot_text_size",
+                        .div_inline_block(6, shiny::sliderInput("gwes_axis_size", "Axis text size:", min = 6, max = 30, value = 14, step = 1)),
+                        .div_inline_br_block(0.5),
+                        .div_inline_block(6, shiny::sliderInput("gwes_title_size", "Axis title size:", min = 6, max = 30, value = 14, step = 1)),
+                        .div_inline_br_block(0.5),
+                        .div_inline_block(6, shiny::sliderInput("gwes_legend_size", "Legend size:", min = 6, max = 30, value = 14, step = 1))
+                    ),
+                    shiny::conditionalPanel(
+                        condition = "input.show_gwes_plot_colors",
+                        .div_inline_block(6, colourpicker::colourInput("gwes_color_direct", "Direct color:", value = "steelblue")),
+                        .div_inline_br_block(0.5),
+                        .div_inline_block(6, colourpicker::colourInput("gwes_color_indirect", "Indirect color:", value = "grey")),
+                        .div_inline_br_block(0.5),
+                        .div_inline_block(6, colourpicker::colourInput("gwes_selection_color", "Selection color:", value = "red")),
+                        .div_inline_br_block(0.5),
+                    ),
+                    shiny::br(), shiny::br(),
                     shiny::plotOutput("manhattan_plot",
                                       width = "30cm",
                                       height = "10cm",
@@ -8,11 +38,11 @@
                                       brush = shiny::brushOpts(id = "manhattan_plot_brush", resetOnNew = TRUE),
                                       dblclick = "manhattan_plot_double_click"
                     ),
-                    shiny::br(), shiny::br(),
-                    shiny::h4("Select rows in the table on the left. The corresponding pairs are plotted in red in the scatter plot."),
-                    shiny::h4("Select an area and double click in it to zoom in. Double click to zoom back out."),
-                    shiny::h4("Click on (or near) a black point to see the corresponding data in the table below:"),
                     shiny::br(),
-                    shiny::tableOutput("manhattan_plot_table")
+                    shiny::tableOutput("manhattan_plot_table"),
+                    shiny::br(),
+                    shiny::h4("Select rows in the table on the left. The corresponding pairs are highlighted in the scatter plot."),
+                    shiny::h4("Select an area and double click in it to zoom in. Double click to zoom back out."),
+                    shiny::h4("Click on (or near) a direct point to aggregate the corresponding data into the table above.")
     )
 }
