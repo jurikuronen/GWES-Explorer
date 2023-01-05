5336a9b0d1bbcc2e524ed86edd742d518cfdc673
Juri Kuronen <juri.kuronen@gmail.com>
2023-01-05 12:26:40 +0200

    Add new RcppExports.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // create_pos_links
 Rcpp::DataFrame create_pos_links(Rcpp::List outliers_direct, Rcpp::List pos_data);
 RcppExport SEXP _GWESExplorer_create_pos_links(SEXP outliers_directSEXP, SEXP pos_dataSEXP) {
