3913c8e8d2be3c395034ee9b0dd3905eb1af4f88
Juri Kuronen <juri.kuronen@gmail.com>
2023-01-11 16:42:13 +0200

    Increase maximum file size limit to 512MB.

--- R/GWESExplorer.R
+++ R/GWESExplorer.R
@@ -8,7 +8,7 @@
 #'
 #' @export launch_GWESExplorer
 launch_GWESExplorer <- function(launch_in_browser = TRUE) {
-    options(shiny.maxRequestSize = 32 * 1024 * 1024) # Increase maximum file size limit to 32MB.
+    options(shiny.maxRequestSize = 512 * 1024 * 1024) # Increase maximum file size limit to 512MB.
     .initialize_data_keys()
     ui <- .create_ui()
     shiny::shinyApp(ui, .server, options = list(launch.browser = launch_in_browser))
