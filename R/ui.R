5a6615cb06634f8bce1ed430b85e088162e0a918
Juri Kuronen <juri.kuronen@gmail.com>
2026-08-21 01:38:53 +0300

    Docs: Update the About tab

--- R/ui.R
+++ R/ui.R
@@ -1,15 +1,13 @@
 # Create the UI definition of the Shiny app.
 .create_ui_layout <- function() {
-    version <- as.character(utils::packageVersion("GWESExplorer"))
-
     shinyjs::useShinyjs()
     shiny::fluidPage(
         shiny::navbarPage(
-            title = paste0("GWES-Explorer v", version),
+            title = "GWES-Explorer",
             id = "tabs",
-            .ui_about_tab_panel(version),
+            .ui_about_tab_panel(),
             .ui_upload_data_tab_panel(),
-            .ui_analyse_data_tab_panel()
+            .ui_analyze_data_tab_panel()
         )
     )
 }
--- tests/testthat/test_ui.R
+++ tests/testthat/test_ui.R
@@ -1,6 +1,6 @@
-test_that(".create_ui_layout displays the package version in the navbar", {
+test_that(".create_ui_layout displays the package version", {
     version <- as.character(utils::packageVersion("GWESExplorer"))
     html <- htmltools::renderTags(.create_ui_layout())$html
 
-    expect_match(html, paste0("GWES-Explorer v", version), fixed = TRUE)
+    expect_match(html, paste("Version", version), fixed = TRUE)
 })
