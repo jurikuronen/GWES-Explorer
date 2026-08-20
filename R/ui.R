528181b9ecc65ca6ce4df3ae686d26f12d062b9c
Juri Kuronen <juri.kuronen@gmail.com>
2026-08-21 01:55:53 +0300

    Chore: Clean up the main UI

--- R/ui.R
+++ R/ui.R
@@ -1,11 +1,11 @@
-# Create the UI definition of the Shiny app.
+# Creates the application's top-level browser interface.
 .create_ui_layout <- function() {
     shiny::fluidPage(
         # Set up the Shiny app to use shinyjs; required for the shinyjs::reset() calls to work.
         shinyjs::useShinyjs(),
+        # Display the application's main sections as tabs in a navigation bar at the top of the page.
         shiny::navbarPage(
             title = "GWES-Explorer",
-            id = "tabs",
             .ui_about_tab_panel(),
             .ui_upload_data_tab_panel(),
             .ui_analyze_data_tab_panel()
