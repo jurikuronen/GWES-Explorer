e7981590b75b1fed5f0f19bc2889dcfd1ac7dd58
Juri Kuronen <juri.kuronen@gmail.com>
2026-08-26 22:36:16 +0300

    Refactor: Clean up status helpers

--- R/status.R
+++ R/status.R
@@ -1,11 +1,17 @@
-# Status constants.
+# Status codes.
 .STATUS_SUCCESS <- 0
 .STATUS_FAILURE <- 1
 
+# Escapes text for HTML.
 .escape_html <- function(text) {
-    htmltools::htmlEscape(as.character(text))
+    htmltools::htmlEscape(text)
 }
 
-.status <- function(result, status_msg) {
-    return(data.frame(success = result, status = shiny::HTML(status_msg)))
+# Creates a status result.
+.status <- function(status_code, status_message) {
+    data.frame(
+        success = status_code,
+        # Use HTML formatting in status messages.
+        status = shiny::HTML(status_message)
+    )
 }
