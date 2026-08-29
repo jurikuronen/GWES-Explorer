0ec463a8d48de346de57ef28992da835020aad7a
Juri Kuronen <juri.kuronen@gmail.com>
2026-08-30 02:02:22 +0300

    Docs: Correct documentation on max_request_size

--- R/GWESExplorer.R
+++ R/GWESExplorer.R
@@ -3,17 +3,14 @@
 #' @description Launch the Shiny app for GWES-Explorer.
 #'
 #' @param launch_in_browser Whether to launch GWES-Explorer in the user's default browser. Defaults to TRUE.
-#' @param max_request_size Maximum input file size in bytes. Defaults to 32 MiB.
+#' @param max_request_size Maximum web request size in bytes, which limits file uploads. Defaults to 32 MiB.
+#'                         This changes the global shiny.maxRequestSize option, which resets when the R session ends.
 #'
 #' @return A Shiny application object.
 #'
 #' @export launch_GWESExplorer
-launch_GWESExplorer <- function(launch_in_browser = TRUE,
-                                max_request_size = 32 * 1024 * 1024)
-{
-    options(shiny.maxRequestSize = max_request_size) # Set maximum file size limit.
+launch_GWESExplorer <- function(launch_in_browser = TRUE, max_request_size = 32 * 1024 * 1024) {
+    options(shiny.maxRequestSize = max_request_size)
 
-    shiny::shinyApp(.create_ui_layout(),
-                    .server,
-                    options = list(launch.browser = launch_in_browser))
+    shiny::shinyApp(.create_ui_layout(), .server, options = list(launch.browser = launch_in_browser))
 }
